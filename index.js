18a3fc7c3f5211dc2f1b9f9031655c15fa05c31c
Adrián Iglesias Fernández <72018196+IgleDev@users.noreply.github.com>
2023-03-30 02:05:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,85 +2,96 @@ let input = document.getElementById("inputText");
 let btnAdd = document.getElementById("buttonAdd");
 let inputFolder = document.getElementById("inputTextFolder");
 let btnFolderAdd = document.getElementById("buttonFolderAdd");
-let adminTareas = document.getElementById('opc-tareas');
-let selectsOPC = document.getElementById('folders');
-let opcSelec = document.getElementById('folders').options;
-let titulo = document.getElementById('tituloCarpeta');
+let adminTareas = document.getElementById("opc-tareas");
+let selectsOPC = document.getElementById("folders");
+let opcSelec = document.getElementById("folders").options;
+let titulo = document.getElementById("tituloCarpeta");
 
 // * Mostramos el input para añadir la carpeta
-function mostrar(){
-    document.getElementById('addFolder').style.display = 'flex';
+function mostrar() {
+    document.getElementById("addFolder").style.display = "flex";
 }
 
 // * Añadimos una tarea a la lista
 btnAdd.addEventListener("click", (e) => {
-    e.preventDefault;
+    e.preventDefault();
     let tarea = input.value;
     let litexto = document.createElement("p");
     litexto.textContent = tarea;
-    let carpeta = selectsOPC.value; 
-    if(tarea !== ""){
-        if(selectsOPC.value === carpeta){
-            let lisS = document.createElement("li");
-            let liStexto = document.createElement("p");
-            liStexto.textContent = tarea + ' - ' + carpeta;
-            if(selectsOPC.value === carpeta){
-                lisS.appendChild(liStexto);
-                lisS.appendChild(botonBorrarListas());
-                let ulsC = document.createElement("ul")
-                ulsC.appendChild(lisS);
-                ulsC.id = 'listas-'+carpeta;
-                ulsC.dataset = carpeta;
-                ulsC.className = 'tareas'
-                adminTareas.appendChild(ulsC);
-            }
-        }
-    }
+    let carpeta = selectsOPC.value;
+
+    if (tarea === "") return;
+
+    if (selectsOPC.value !== carpeta) return;
+
+    let lisS = document.createElement("li");
+    let liStexto = document.createElement("p");
+    liStexto.textContent = tarea;
+
+    lisS.appendChild(liStexto);
+    lisS.appendChild(botonBorrarListas());
+    let ulsC = document.createElement("ul");
+    ulsC.appendChild(lisS);
+    ulsC.classList.add("listas-" + carpeta);
+    ulsC.classList.add("tareas");
+    adminTareas.appendChild(ulsC);
+
     input.value = "";
-})
+});
 
 // * Añadimos una carpeta a la lista + el radio
 btnFolderAdd.addEventListener("click", (e) => {
-    e.preventDefault;
-    let carpeta2 = inputFolder.value
-    if(carpeta2 !== ""){
-       let opc = document.createElement('option');
-       opcTexto = document.createTextNode(carpeta2);
-       opc.appendChild(opcTexto);
-       opc.id = 'lista-'+carpeta2;
-       opc.value = carpeta2;
-       selectsOPC.appendChild(opc);
-    }
-    inputFolder.value = "";
-})
+    e.preventDefault();
+    let carpeta2 = inputFolder.value;
 
-function botonBorrarListas(){
-    // ! Creamos el boton para borrar 
-    let btnDeleteSub = document.createElement("button") 
+    inputFolder.value = "";
+
+    if (carpeta2 === "") return;
+
+    let opc = document.createElement("option");
+    opcTexto = document.createTextNode(carpeta2);
+    opc.appendChild(opcTexto);
+    opc.id = "lista-" + carpeta2;
+    opc.value = carpeta2;
+    selectsOPC.appendChild(opc);
+    document.getElementById("addFolder").style.display = "none";
+});
+
+function botonBorrarListas() {
+    // ! Creamos el boton para borrar
+    let btnDeleteSub = document.createElement("button");
     btnDeleteSub.textContent = "X";
     btnDeleteSub.className = "buttonDelete";
     btnDeleteSub.type = "button";
 
     btnDeleteSub.addEventListener("click", (e) => {
         let tareaBorrar = adminTareas.childNodes[0];
-        adminTareas.removeChild(tareaBorrar)
-    })
+        adminTareas.removeChild(tareaBorrar);
+    });
     return btnDeleteSub;
 }
 
-selectsOPC.addEventListener("change", function() {
+selectsOPC.addEventListener("change", function () {
     let tituloSelect = opcSelec[selectsOPC.selectedIndex].text;
     titulo.textContent = tituloSelect;
 });
 
-selectsOPC.addEventListener('change', function(){
-    let opc = document.getElementById('listas-' + selectsOPC.value);
-    let listas = document.getElementsByTagName("ul");;
-    for(let i = 0; i < listas.length;i++){
-        if(listas[i] === opc){
-            listas[i].style.display = "block";
-        }else{
-            listas[i].style.display = "none";
+selectsOPC.addEventListener("change", function () {
+    const className = "listas-" + selectsOPC.value;
+
+    let listas = document.getElementsByTagName("ul");
+
+    for (let i = 0; i < listas.length; i++) {
+        const ul = listas.item(i);
+
+        if (ul === null) break;
+
+        if (ul.classList.contains(className)) {
+            ul.style.display = "block";
+            continue;
         }
+
+        ul.style.display = "none";
     }
+    
 });
